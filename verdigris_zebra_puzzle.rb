0348cd11d84c4854cc4d26ba1c43ac8871d5a39b
William Chan <William@Williams-MacBook-Air.local>
2013-09-03 21:28:41 -0700

    revise keywords hash to map to type of attribute e.g. color, pet, etc

--- verdigris_zebra_puzzle.rb
+++ verdigris_zebra_puzzle.rb
@@ -1,55 +1,78 @@
 #! /usr/bin/env ruby
 
 class House
-  attr_accessor :position, :color, :nationality, :drink, :smoke, :pet
+  attr_accessor :number, :color, :nationality, :drink, :smoke, :pet
+
+  def self.find(keyword)
+    #find the house by keyword (e.g. House.find("japanese"))
+  end
 
 end
 
 class Solution
   def initialize
-    @houses = {}
-    5.times do |idx|
-      @houses[idx] = House.new 
+    @houses = []
+    create_keywords_hash
+  end
+
+  def create_keywords_hash
+    position_words_arr = ['first', 'middle', 'right', 'next']
+    color_words_arr = ['yellow', 'blue', 'red', 'ivory', 'green']
+    nationality_words_arr = 
+    ['norwegian', 'ukrainian', 'englishman', 'spaniard', 'japanese']
+    drink_words_arr = ['tea', 'milk', 'orange', 'coffee']
+    smoke_words_arr = 
+    ['kools', 'chesterfields', 'old', 'lucky', 'parliaments']
+    pet_words_arr = ['fox', 'horse', 'snails', 'dog']
+
+    @key_words_hash = Hash.new(false)
+
+    [position_words_arr, color_words_arr, nationality_words_arr, drink_words_arr, smoke_words_arr, pet_words_arr].each_with_index do 
+      |array, idx|
+
+      attributes = [:number, :color, :nationality, :drink, :smoke, :pet]
+      array.each do |word|
+        @key_words_hash[word] = attributes[idx]
+      end
     end
+    p @key_words_hash
   end
 
   def render_solution
-    @houses.each_value do |house|
+    @houses.each do |house|
       p house
     end
   end
 
   def parse_puzzle_constraints
-    position_words_arr = ['first', 'middle', 'right', 'next']
-    nationality_words_arr = 
-    ['norweigian', 'ukrainian', 'englishman', 'spaniard', 'japanese']
-    drink_words_arr = ['tea', 'milk', 'orange', 'coffee']
-    smoke_words_arr = 
-    ['kools', 'chesterfield', 'old gold', 'lucky', 'parliament']
-    pet_words_arr = ['fox', 'horse', 'snails', 'dog']
-
-    key_words_hash = Hash.new(false)
-
-    [position_words_arr, nationality_words_arr, drink_words_arr, smoke_words_arr, pet_words_arr].each do |array|
-
-      array.each do |word|
-        key_words_hash[word] = true
-      end
-    end
-
-    
-    p key_words_hash
+    simple_relations = {}
+    next_relations = {}
+    right_relations = {}
 
     File.open('puzzle_constraints.txt').each_line do |line|
+      matched_words = []
+      line.chomp[0..-2].split(" ").each do |word|
+        matched_words << word if @key_words_hash[word.downcase]
+      end
+
+      if matched_words.length == 2
+        simple_relations[matched_words.first] = matched_words.last
+      elsif matched_words.include?("next")
+        next_relations[matched_words.first] = matched_words.last
+      elsif matched_words.include?("right")
+        right_relations[matched_words.first] = matched_words.last
+      end
 
     end
 
+    p simple_relations, next_relations, right_relations
   end
+
 end
 
 
 s = Solution.new
-s.parse_puzzle_constraints
-s.render_solution
+# s.parse_puzzle_constraints
+# s.render_solution
 
 #script to run algorithm
