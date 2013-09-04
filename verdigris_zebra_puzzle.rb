06fe904d91c53af31c24bed131d0e128195b757c
William Chan <William@Williams-MacBook-Air.local>
2013-09-03 21:39:33 -0700

    add House.find method to find house by attribute

--- verdigris_zebra_puzzle.rb
+++ verdigris_zebra_puzzle.rb
@@ -2,20 +2,23 @@
 
 class House
   attr_accessor :number, :color, :nationality, :drink, :smoke, :pet
+  @@houses = []
+
+  def initialize
+    @@houses << self
+  end
 
   def self.find(keyword)
     #find the house by keyword (e.g. House.find("japanese"))
+    @@houses.each do |house|
+      attribute = $key_words_hash[keyword]
+      return house if house.send(attribute) == keyword
+    end
+    return nil
   end
-
 end
 
-class Solution
-  def initialize
-    @houses = []
-    create_keywords_hash
-  end
-
-  def create_keywords_hash
+def create_keywords_hash
     position_words_arr = ['first', 'middle', 'right', 'next']
     color_words_arr = ['yellow', 'blue', 'red', 'ivory', 'green']
     nationality_words_arr = 
@@ -25,17 +28,22 @@ class Solution
     ['kools', 'chesterfields', 'old', 'lucky', 'parliaments']
     pet_words_arr = ['fox', 'horse', 'snails', 'dog']
 
-    @key_words_hash = Hash.new(false)
+    $key_words_hash = Hash.new(false)
 
     [position_words_arr, color_words_arr, nationality_words_arr, drink_words_arr, smoke_words_arr, pet_words_arr].each_with_index do 
       |array, idx|
 
       attributes = [:number, :color, :nationality, :drink, :smoke, :pet]
       array.each do |word|
-        @key_words_hash[word] = attributes[idx]
+        $key_words_hash[word] = attributes[idx]
       end
     end
-    p @key_words_hash
+    $key_words_hash
+  end
+
+class Solution
+  def initialize
+    @houses = []
   end
 
   def render_solution
@@ -51,7 +59,7 @@ class Solution
 
     File.open('puzzle_constraints.txt').each_line do |line|
       matched_words = []
-      line.chomp[0..-2].split(" ").each do |word|
+      line.chomp[0...-1].split(" ").each do |word|
         matched_words << word if @key_words_hash[word.downcase]
       end
 
@@ -71,7 +79,15 @@ class Solution
 end
 
 
-s = Solution.new
+create_keywords_hash
+
+# h = House.new
+# h.color = 'red'
+# h2 = House.new
+# h2.color = 'blue'
+# p House.find('red')
+
+# s = Solution.new
 # s.parse_puzzle_constraints
 # s.render_solution
 
