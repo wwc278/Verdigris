11dd7a39fda0f1530bdd54b77462f0bb7a1d1af5
William Chan <William@Williams-MacBook-Air.local>
2013-09-04 16:50:00 -0700

    debug violates_right_relations? method to provide the correct deduction that the first house is yellow

--- verdigris_zebra_puzzle.rb
+++ verdigris_zebra_puzzle.rb
@@ -1,5 +1,7 @@
 #! /usr/bin/env ruby
 
+require 'debugger'
+
 def create_keywords_hash
   $position_words_arr = ['first', 'middle', 'right', 'next']
   $color_words_arr = ['yellow', 'blue', 'red', 'ivory', 'green']
@@ -30,6 +32,14 @@ class House
   def initialize
     #TODO: create mass assignment?
     @@houses << self
+  end
+
+  def self.find(keyword)
+    attribute = $keywords_hash[keyword]
+    @@houses.each do |house|
+      return house if house.send(attribute) == keyword
+    end
+    nil
   end
 
   def self.find_or_create(keyword)
@@ -83,6 +93,8 @@ class Solution
   end
 
   def render_solution
+    puts ""
+    puts "final solution:"
     @final_ordered_houses.each do |house|
       p house
     end
@@ -107,6 +119,7 @@ class Solution
       elsif matched_words.include?("right")
         @right_relations[matched_words.first] = matched_words.last
       elsif matched_words.length == 2
+        # map relations both ways to allow for reverse look-up
         @simple_relations[matched_words.first] = matched_words.last
         @simple_relations[matched_words.last] = matched_words.first
       end
@@ -114,9 +127,13 @@ class Solution
     end
 
     puts "simple_relations: #{@simple_relations}"
+    puts ""
     puts "next_relations: #{@next_relations}"
+    puts ""
     puts "right_relations: #{@right_relations}"
+    puts ""
     puts "pos_relations: #{@pos_relations}"
+    puts ""
 
     [@simple_relations, @next_relations, @right_relations, @pos_relations]
   end
@@ -143,10 +160,10 @@ class Solution
 
       attribute = $keywords_hash[value].to_s
       if value == "middle"
-        curr_house.send(attribute + "=", 2)
+        curr_house.send(attribute + "=", "middle")
         @final_ordered_houses[2] = curr_house
       elsif value == "first"
-        curr_house.send(attribute + "=", 0)
+        curr_house.send(attribute + "=", "first")
         @final_ordered_houses[0] = curr_house
       end
     end
@@ -161,17 +178,18 @@ class Solution
   def place_next_relations
     # go through "next relations" (e.g. norwegian next to blue house)
     @next_relations.each do |key, value|
-      curr_house = House.find_or_create(key)
+      curr_house = House.find(key) if House.exist?(key)
+
       if curr_house
         new_house = House.new
         attribute = $keywords_hash[value].to_s
 
-        if curr_house.number == 0
-          new_house.number = 1
+        if curr_house.number == "first"
+          new_house.number = "second"
           new_house.send(attribute + "=", value)
           @final_ordered_houses[1] = new_house
-        elsif curr_house.number == 4
-          new_house.number = 3
+        elsif curr_house.number == "last"
+          new_house.number = "fourth"
           new_house.send(attribute + "=", value)
           @final_ordered_houses[3] = new_house
         end
@@ -190,22 +208,25 @@ class Solution
 
   def deduce_color
     @final_ordered_houses.each do |house|
-      bool_arr = []
       next if house.nil? # only try to deduce color if the house has some info
+      bool_arr = []
 
       if house.color.nil? #try to deduce color if house has no color
         available_colors.each do |color|
           house.color = color
+          p house, violates_relations?
+
           bool_arr << violates_relations?
           house.color = nil
         end
       end
-
+      p bool_arr
     end
   end
 
   def violates_relations?
     @final_ordered_houses.each do |house|
+      next unless house #skip if house is nil
       $attributes.each do |attribute|
         keyword = house.send(attribute)
         # skip if attribute is nil (e.g. house.color returns nil)
@@ -215,37 +236,89 @@ class Solution
         return true if violates_simple_relations?(keyword)
 
         #check if it violates next relations
-
+        return true if violates_right_relations?
 
         #check if it violates right relations
 
       end
     end
+    false
   end
 
   def violates_simple_relations?(keyword)
-    matching_keyword = $simple_relations[keyword]
-    # does not violate relations both houses being compared exists
-    return false unless House.exist?(keyword) && House.exist?(matching_keyword)
+    matching_keyword = @simple_relations[keyword]
+    # if matching_keyword is nil, return false (i.e. keyword is not a simple relation)
+    return false unless matching_keyword
 
-    # below will not create houses as both have been checked to exist
-    house1 = House.find_or_create(keyword)
-    house2 = House.find_or_create(matching_keyword)
-    return false if house1 == house2
+    # if house with keyword exists, find the house and compare the matching attribute to the matching keyword
 
-    true
+    if House.exist?(keyword)
+      curr_house = House.find(keyword)
+      attribute = $keywords_hash[matching_keyword]
+      return false if curr_house.send(attribute).nil?
+      return true unless curr_house.send(attribute) == matching_keyword
+    end
+    false
   end
+end
 
+def violates_right_relations?
+  #loop through all right relations, in this case it is unecessary because there is only one, but it allows for a different problem with multiple right relations to be solved
+  @right_relations.each do |key, value|
+
+    if House.exist?(key)
+      curr_house = House.find(key)
+      r_idx = @final_ordered_houses.index(curr_house)
+
+      # true if "key" house (e.g green) is first
+      if curr_house.number == 'first'
+        return true
+      else
+        left_house = @final_ordered_houses[r_idx - 1] 
+        # return true unless the left house is nil or has the correct color
+        unless left_house.nil? || left_house.color == value
+          return true
+        end
+      end
+    end
+
+    if House.exist?(value)
+      curr_house = House.find(value)
+      l_idx = @final_ordered_houses.index(curr_house)
+
+      # true if "value" house (e.g. ivory) is last
+      if curr_house.number == 'last'
+        return true
+      else
+        right_house = @final_ordered_houses[l_idx + 1]
+        # return true unless the right house is nil or has the correct color
+        unless right_house.nil? || right_house.color == key
+          return true
+        end
+      end
+      
+    end
+
+    # true if index of key house minus index of value house is greater than 1
+    if House.exist?(key) && House.exist?(value)
+      house_r = House.find(key)
+      house_l = House.find(value)
+      r_idx = @final_ordered_houses.index(house_r)
+      l_idx = @final_ordered_houses.index(house_l)
+      return true if r_idx - l_idx > 1
+    end
+  end
+  false
 end
 
 #script to run algorithm
 
-# create_keywords_hash
+create_keywords_hash
 
-# s = Solution.new
-# s.parse_puzzle_constraints
-# s.make_deductions
-# s.render_solution
+s = Solution.new
+s.parse_puzzle_constraints
+s.make_deductions
+s.render_solution
 
 # work on parsing the next and right relations
 # might have to iterate through random choices to continue with the algorithm
