fc284a3e441706e7daa8072dc4e05579a8f24568
William Chan <William@Williams-MacBook-Air.local>
2013-09-03 22:10:53 -0700

    create houses from simple relations, end up with 8 houses with correct simple relations

--- verdigris_zebra_puzzle.rb
+++ verdigris_zebra_puzzle.rb
@@ -11,7 +11,7 @@ class House
   def self.find(keyword)
     #find the house by keyword (e.g. House.find("japanese"))
     @@houses.each do |house|
-      attribute = $key_words_hash[keyword]
+      attribute = $keywords_hash[keyword]
       return house if house.send(attribute) == keyword
     end
     return nil
@@ -19,27 +19,27 @@ class House
 end
 
 def create_keywords_hash
-    position_words_arr = ['first', 'middle', 'right', 'next']
-    color_words_arr = ['yellow', 'blue', 'red', 'ivory', 'green']
-    nationality_words_arr = 
-    ['norwegian', 'ukrainian', 'englishman', 'spaniard', 'japanese']
-    drink_words_arr = ['tea', 'milk', 'orange', 'coffee']
-    smoke_words_arr = 
-    ['kools', 'chesterfields', 'old', 'lucky', 'parliaments']
-    pet_words_arr = ['fox', 'horse', 'snails', 'dog']
+  position_words_arr = ['first', 'middle', 'right', 'next']
+  color_words_arr = ['yellow', 'blue', 'red', 'ivory', 'green']
+  nationality_words_arr = 
+  ['norwegian', 'ukrainian', 'englishman', 'spaniard', 'japanese']
+  drink_words_arr = ['tea', 'milk', 'orange', 'coffee']
+  smoke_words_arr = 
+  ['kools', 'chesterfields', 'old', 'lucky', 'parliaments']
+  pet_words_arr = ['fox', 'horse', 'snails', 'dog']
 
-    $key_words_hash = Hash.new(false)
+  $keywords_hash = Hash.new(false)
 
-    [position_words_arr, color_words_arr, nationality_words_arr, drink_words_arr, smoke_words_arr, pet_words_arr].each_with_index do 
-      |array, idx|
+  [position_words_arr, color_words_arr, nationality_words_arr, drink_words_arr, smoke_words_arr, pet_words_arr].each_with_index do 
+    |array, idx|
 
-      attributes = [:number, :color, :nationality, :drink, :smoke, :pet]
-      array.each do |word|
-        $key_words_hash[word] = attributes[idx]
-      end
+    attributes = [:number, :color, :nationality, :drink, :smoke, :pet]
+    array.each do |word|
+      $keywords_hash[word] = attributes[idx]
     end
-    $key_words_hash
   end
+  $keywords_hash
+end
 
 class Solution
   def initialize
@@ -56,39 +56,53 @@ class Solution
     simple_relations = {}
     next_relations = {}
     right_relations = {}
+    pos_relations = {}
 
     File.open('puzzle_constraints.txt').each_line do |line|
       matched_words = []
       line.chomp[0...-1].split(" ").each do |word|
-        matched_words << word if @key_words_hash[word.downcase]
+        matched_words << word.downcase if $keywords_hash[word.downcase]
       end
 
-      if matched_words.length == 2
-        simple_relations[matched_words.first] = matched_words.last
+      if matched_words.include?("middle") || matched_words.include?("first")
+        pos_relations[matched_words.first] = matched_words.last
       elsif matched_words.include?("next")
         next_relations[matched_words.first] = matched_words.last
       elsif matched_words.include?("right")
         right_relations[matched_words.first] = matched_words.last
+      elsif matched_words.length == 2
+        simple_relations[matched_words.first] = matched_words.last
       end
 
     end
 
-    p simple_relations, next_relations, right_relations
+    create_from_simple_relations(simple_relations)
+
+    [simple_relations, next_relations, right_relations, pos_relations]
+  end
+
+  def create_from_simple_relations(simple_relations)
+    simple_relations.each do |key, value|
+      curr_house = House.find(key)
+      unless curr_house
+        curr_house = House.new
+        attribute = $keywords_hash[key].to_s
+        curr_house.send(attribute + "=", key)
+        @houses << curr_house
+      end
+
+      attribute = $keywords_hash[value].to_s
+      curr_house.send(attribute + "=", value)
+    end
   end
 
 end
 
+#script to run algorithm
 
 create_keywords_hash
 
-# h = House.new
-# h.color = 'red'
-# h2 = House.new
-# h2.color = 'blue'
-# p House.find('red')
+s = Solution.new
+s.parse_puzzle_constraints
+s.render_solution
 
-# s = Solution.new
-# s.parse_puzzle_constraints
-# s.render_solution
-
-#script to run algorithm
